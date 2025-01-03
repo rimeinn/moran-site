29142436357424b344447ca4878e02b07d66b3a5
ksqsf <i@ksqsf.moe>
2025-01-03 17:25:59 +0100

    fix: zh-Hans public

--- .vitepress/scripts/simplify.sh
+++ .vitepress/scripts/simplify.sh
@@ -5,4 +5,5 @@ mkdir zh-Hans
 cp index.md zh-Hans
 cp -a lookup zh-Hans
 cp -a book zh-Hans
+cp -a public zh-Hans
 find zh-Hans -name '*.md' -exec bash './.vitepress/scripts/simplify_in_place.sh' \{\} \;
